f452dcc2fa21a3d665afa1ad74097f737d09aaa2
BenPearo <35578281+BenPearo@users.noreply.github.com>
2020-02-07 18:05:27 -0500

    Add files via upload

---
+++ ex1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    char *buf;
+    buf = malloc(9999999999);
+    printf("Enter some text: ");
+    fgets(buf, 1024, stdin);
+    printf("You wrote: %s\n", buf);
+    return 0;
+}
